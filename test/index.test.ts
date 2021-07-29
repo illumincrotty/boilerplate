868528e942783e53a32e5ffd8d2e5dd804d1f4e9
Brian_Crotty <5673408+illumincrotty@users.noreply.github.com>
2021-07-29 12:06:44 -0500

    Updated dependencies

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,38 +1,33 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import test from 'ava';
-import { fiveFunction, identity } from '../src/index';
-
-const one = 1,
-	two = 2,
-	five = 5;
+import { example } from '../src/example';
 
 test('passing test', (t) => {
 	t.pass();
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-const valueExample = one ? true : false;
 test('should know truth values', (t) => {
-	t.true(valueExample);
+	t.true(true);
 });
 
 test('should know 1 == 1', (t) => {
-	t.is(one, one);
-	t.is(one, one);
+	t.is(1, 1);
+	t.is(1, 1);
 });
 
 test('should work with named imports', (t) => {
 	const test = { property: 7 };
-	t.deepEqual(identity(test)(), test);
-	t.is(one, one);
-	t.is(two, two);
+	t.deepEqual(example.identity(test)(), test);
+	t.is(1, 1);
+	t.is(2, 2);
 });
 
 test('can achieve full coverage', (t) => {
-	t.is(fiveFunction(), five);
+	t.is(example.fiveFunction(), 5);
 });
 
 test('can dynamically import fs', async (t) => {
 	const fs = (await import('node:fs')).promises;
 	const text = await fs.readFile('./test/example.txt');
-	t.is(text.BYTES_PER_ELEMENT, one);
+	t.is(text.BYTES_PER_ELEMENT, 1);
 });
